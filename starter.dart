35fd86a18c8d60b0b617685295ca070ca467616c
M-ASHaR <itisashar3007@gmail.com>
2023-02-19 03:11:07 +0500

    first day with dart programming

---
+++ starter.dart
@@ -0,0 +1,15 @@
+void main() {
+  print('Welcome to dart');
+
+  var ashar = new Human(); //creating a Class object
+
+  // declaration af any variable
+  //? means it can be nullable
+  int? a; // can only be called in same class or scope
+  a = 5; //assigning value
+  print(a);
+}
+
+class Human {
+  Human();
+}
